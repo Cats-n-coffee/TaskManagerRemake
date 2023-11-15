84cc92361934e2ea50533293132c618d0e337b70
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-11-14 21:50:33 -0500

    First solution at displaying process list in UI

--- TaskManagerRemake.App/ViewModels/ProcessViewModel.cs
+++ TaskManagerRemake.App/ViewModels/ProcessViewModel.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManagerRemake.Domain.Services.Tabs;
+using TaskManagerRemake.Domain.Models;
+using System.Diagnostics;
 
 namespace TaskManagerRemake.WPF.ViewModels
 {
     public class ProcessViewModel : BaseViewModel
     {
+        private List<ProcessItem> _allProcesses;
+        
+        public List<ProcessItem> AllProcesses
+        {
+            get { return _allProcesses; }
+            set
+            {
+                _allProcesses = value;
+                OnPropertyChanged(nameof(_allProcesses));
+            }
+        }
+        public ProcessViewModel()
+        {
+            Debug.WriteLine("constructor");
+            
+            _allProcesses = ProcessTab.GetProcessesForTab();
+
+            foreach (var item in _allProcesses)
+            {
+                Debug.WriteLine($"name {item.Name} id {item.Id}");
+            }
+        }
     }
 }
