6ca3b06668e89bba6fed5b36cfc682ff440f8b6e
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-12-02 21:31:55 -0500

    Add dynamic stats and extended performanceItemDisplay from ViewBaseModel for timer - SLOW

--- TaskManagerRemake.App/ViewModels/PerformanceItemDisplay.cs
+++ TaskManagerRemake.App/ViewModels/PerformanceItemDisplay.cs
@@ -4,18 +4,20 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Threading;
 using TaskManagerRemake.Domain.Models;
 using TaskManagerRemake.Domain.Services.PerformanceTab;
 
 namespace TaskManagerRemake.WPF.ViewModels
 {
-    public class PerformanceItemDisplay
+    public class PerformanceItemDisplay : ViewModelBase
     {
         private readonly IPerformanceItem selectedTab;
         public string TabTitle { get; set; }
         public string TabSpec { get; set; }
         public ObservableCollection<PerformanceStat> StaticStats { get; set; }
-        // private ObservableCollection<PerformanceStat> _dynamicStats;
+        public ObservableCollection<PerformanceStat> _dynamicStats;
 
         public PerformanceItemDisplay(string tabToDisplay)
         {
@@ -30,6 +32,35 @@ namespace TaskManagerRemake.WPF.ViewModels
             TabTitle = selectedTab.GetTabTitle();
             StaticStats = new ObservableCollection<PerformanceStat>(selectedTab.GetStaticStats());
             TabSpec = selectedTab.GetTabSpecs();
+            DynamicStats = new ObservableCollection<PerformanceStat>(selectedTab.GetDynamicStats());
+
+            InitTimer();
+        }
+
+        private void InitTimer()
+        {
+            DispatcherTimer usageTimer = new DispatcherTimer();
+
+            usageTimer.Tick += new EventHandler(Timer_Tick);
+            usageTimer.Interval = TimeSpan.FromSeconds(1);
+            usageTimer.Start();
+        }
+
+        private void Timer_Tick(Object source, EventArgs e)
+        {
+            DynamicStats = new ObservableCollection<PerformanceStat>(selectedTab.GetDynamicStats());
+
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public ObservableCollection<PerformanceStat> DynamicStats
+        {
+            get => _dynamicStats;
+            set
+            {
+                _dynamicStats = value;
+                OnPropertyChanged(nameof(DynamicStats));
+            }
         }
     }
 }
