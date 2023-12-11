fb442b89a6ee0fe6e84ad2de1637f043ad8e8855
Cats-n-coffee <lucie.macaigne@gmail.com>
2023-12-10 21:22:23 -0500

    Add colors, clean up performance view model

--- TaskManagerRemake.App/ViewModels/PerformanceViewModel.cs
+++ TaskManagerRemake.App/ViewModels/PerformanceViewModel.cs
@@ -9,24 +9,15 @@ using TaskManagerRemake.Domain.Services.PerformanceTab;
 using System.Windows.Threading;
 using System.Windows.Input;
 
+
 namespace TaskManagerRemake.WPF.ViewModels
 {
     public class PerformanceViewModel : ViewModelBase
     {
         public ObservableCollection<PerformanceItemDisplay> PerformanceItems { get; set; }
-        // private ObservableCollection<PerformanceStat> _dynamicStats;
-        public Dictionary<string, int> FakeData { get; set; }
+ 
         public PerformanceViewModel()
         {
-            FakeData = new Dictionary<string, int>
-            {
-                { "aaa", 34 },
-                { "ggg", 23 },
-                { "ppp", 12 },
-                { "oo", 3 },
-                { "ss", 4 }
-            };
-
             PerformanceItems = new ObservableCollection<PerformanceItemDisplay>(
                 new List<PerformanceItemDisplay>() 
                 {
@@ -34,37 +25,6 @@ namespace TaskManagerRemake.WPF.ViewModels
                     new PerformanceItemDisplay("memory"),
                 }
                 );
-            /*
-
-            InitTimer();
-
-            DynamicStats = new ObservableCollection<PerformanceStat>(performanceItem.GetDynamicStats());*/
         }
-        /*
-        private void InitTimer()
-        {
-            DispatcherTimer usageTimer = new DispatcherTimer();
-
-            usageTimer.Tick += new EventHandler(Timer_Tick);
-            usageTimer.Interval = TimeSpan.FromSeconds(1);
-            usageTimer.Start();
-        }
-
-        private void Timer_Tick(Object source, EventArgs e)
-        {
-            DynamicStats = new ObservableCollection<PerformanceStat>(performanceItem.GetDynamicStats());
-
-            CommandManager.InvalidateRequerySuggested();
-        }
-        
-        public ObservableCollection<PerformanceStat> DynamicStats
-        {
-            get => _dynamicStats;
-            set
-            {
-                _dynamicStats = value;
-                OnPropertyChanged(nameof(DynamicStats));
-            }
-        }*/
     }
 }
